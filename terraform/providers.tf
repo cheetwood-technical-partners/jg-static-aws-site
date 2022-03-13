9664d0be9d363a1f0cb95145cb0b71d00256c328
jaycheetwood <jaremy@hashicorp.com>
2022-03-13 13:49:47 -0700

    Updated files to have appropriate terraform code for creating resources

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,3 +12,12 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,23 @@
 resource "aws_s3_bucket" "www_bucket" {
   bucket = "www.${var.bucket_name}"
-  acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+  tags = var.common_tags
+}
+
+resource "aws_s3_bucket_policy" "www_bucket_policy" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+}
+
+
+resource "aws_s3_bucket_acl" "www_bucket_acl" {
+  bucket = aws_s3_bucket.www_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "www_cors_configuration" {
+  bucket = aws_s3_bucket.www_bucket.bucket
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
@@ -9,24 +25,40 @@ resource "aws_s3_bucket" "www_bucket" {
     allowed_origins = ["https://www.${var.domain_name}"]
     max_age_seconds = 3000
   }
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_website_configuration" "www_bucket_website_configuration" {
+  bucket = aws_s3_bucket.www_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
   }
-
-  tags = var.common_tags
+  error_document {
+    key = "404.html"
+  }
 }
 
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "root_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
 
   tags = var.common_tags
+}
+
+resource "aws_s3_bucket_policy" "root_bucket_policy" {
+  bucket = aws_s3_bucket.root_bucket.id
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+}
+
+resource "aws_s3_bucket_acl" "root_bucket_acl" {
+  bucket = aws_s3_bucket.root_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "root_bucket_website_configuration" {
+  bucket = aws_s3_bucket.root_bucket.bucket
+
+  redirect_all_requests_to {
+    host_name = "https://www.${var.domain_name}"
+  }
 }
