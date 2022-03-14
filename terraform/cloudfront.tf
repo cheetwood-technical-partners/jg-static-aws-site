47a35dec626276f224359117d3b94e6ee89553ee
jaycheetwood <jaremy@hashicorp.com>
2022-03-14 02:47:18 -0700

    Updated files to have appropriate terraform code for creating resources

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,7 +1,7 @@
 # Cloudfront distribution for main s3 site.
 resource "aws_cloudfront_distribution" "www_s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.www_bucket.website_endpoint
+    domain_name = "garrison-gg.s3.amazonaws.com"
     origin_id   = "S3-www-${var.bucket_name}"
 
     custom_origin_config {
@@ -63,8 +63,8 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
 # Cloudfront S3 for redirect to www.
 resource "aws_cloudfront_distribution" "root_s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.root_bucket.website_endpoint
-    origin_id   = "S3-.${var.bucket_name}"
+    domain_name = "garrison-gg.s3.amazonaws.com"
+    origin_id   = "S3-${var.bucket_name}"
     custom_origin_config {
       http_port              = 80
       https_port             = 443
